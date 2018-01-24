376affc124ae453633de653e705dab397032697d
ravid tanami <ravid.tanami@sizmek.com>
2018-01-24 19:06:50 +0200

    Quick Start --> Components and Props --> Composing Components

--- quick-start-app/src/index.js
+++ quick-start-app/src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
-/*function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
-
-ReactDOM.render(
-    <Welcome name={"shu"} />,
-    document.getElementById('root')
-);*/
-
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
 
-const element = <Welcome name="Sara" />;
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara" />
+            <Welcome name="Cahal" />
+            <Welcome name="Edite" />
+        </div>
+    );
+}
+
 ReactDOM.render(
-    element,
+    <App />,
     document.getElementById('root')
 );
