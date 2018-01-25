01517bd7a33f7c2bf4464890db91576f31f0f874
ravid.tanami <ravid.tanami@sizmek.com>
2018-01-25 20:53:29 +0200

    Quick Start --> Conditional Rendering --> Preventing Component from Rendering

--- quick-start-app/src/index.js
+++ quick-start-app/src/index.js
@@ -2,53 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
-class LoginControl extends React.Component {
+function WarningBanner(props) {
+    if (!props.warn) {
+        return null;
+    }
+
+    return (
+        <div className="warning">
+            Warning!
+        </div>
+    );
+}
+
+class Page extends React.Component {
     constructor(props) {
         super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
+        this.state = {showWarning: true}
+        this.handleToggleClick = this.handleToggleClick.bind(this);
     }
 
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
+    handleToggleClick() {
+        this.setState(prevState => ({
+            showWarning: !prevState.showWarning
+        }));
     }
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
         return (
             <div>
-                {isLoggedIn ? (
-                    <LogoutButton onClick={this.handleLogoutClick} />
-                ) : (
-                    <LoginButton onClick={this.handleLoginClick} />
-                )}
+                <WarningBanner warn={this.state.showWarning} />
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
             </div>
         );
     }
 }
 
-function LoginButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Login
-        </button>
-    );
-}
-
-function LogoutButton(props) {
-    return (
-        <button onClick={props.onClick}>
-            Logout
-        </button>
-    );
-}
-
 ReactDOM.render(
-    <LoginControl />,
+    <Page />,
     document.getElementById('root')
 );
