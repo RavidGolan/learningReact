7ac065b852d39ec5e7569204344d23deb9572cd6
ravid.tanami <ravid.tanami@sizmek.com>
2018-01-25 18:06:47 +0200

    Quick Start --> State and Lifecycle --> The Data Flows Down

--- quick-start-app/src/index.js
+++ quick-start-app/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
+function FormattedDate(props) {
+    return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
 class Clock extends React.Component {
     constructor(props) {
         super(props);
@@ -29,7 +33,7 @@ class Clock extends React.Component {
         return (
             <div>
                 <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <FormattedDate date={this.state.date} />
             </div>
         );
     }
