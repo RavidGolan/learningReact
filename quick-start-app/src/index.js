1e3304d16c19f723bcebff41f6ad398ef05abdc6
ravid.tanami <ravid.tanami@sizmek.com>
2018-01-25 20:50:13 +0200

    Quick Start --> Conditional Rendering --> Inline If-Else with Conditional Operator

--- quick-start-app/src/index.js
+++ quick-start-app/src/index.js
@@ -2,22 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 
-function Mailbox(props) {
-    const unreadMessages = props.unreadMessages;
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        return (
+            <div>
+                {isLoggedIn ? (
+                    <LogoutButton onClick={this.handleLogoutClick} />
+                ) : (
+                    <LoginButton onClick={this.handleLoginClick} />
+                )}
+            </div>
+        );
+    }
+}
+
+function LoginButton(props) {
     return (
-        <div>
-            <h1>Hello!</h1>
-            {unreadMessages.length > 0 &&
-            <h2>
-                You have {unreadMessages.length} unread messages.
-            </h2>
-            }
-        </div>
+        <button onClick={props.onClick}>
+            Login
+        </button>
     );
 }
 
-const messages = ['React', 'Re: React', 'Re:Re: React'];
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
 ReactDOM.render(
-    <Mailbox unreadMessages={messages} />,
+    <LoginControl />,
     document.getElementById('root')
 );
